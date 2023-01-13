696e864c6157d84ed29524b6215bbe8a4e361401
mohammad-tanzeel <tanzeel.tech@gmail.com>
2023-01-13 11:30:55 +0400

    Project setup

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { AppModule } from './app.module';
+import cors from 'cors';
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(AppModule);
+  app.use(cors())
+  const config = new DocumentBuilder()
+    .setTitle('Driving Institute')
+    .setDescription('Driving Institute')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  
+  await app.listen(3003);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
